44102c6b616111b5e07a656bdcddd2141caafda0
JoeGCode <122706533+JoeGCode@users.noreply.github.com>
2023-02-22 16:25:03 +0000

    Organise code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,14 +24,29 @@ const Home: FC = () => {
 
   const RESULTS_PER_PAGE = 20;
 
+  //#region Page initialisation and URL params
+
   useEffect(() => {
     homePageInit();
   }, []);
+
+  useEffect(() => {
+    checkAndLoadFromUrlParams();
+  }, [allPokemonNames, searchParams]);
 
   function homePageInit() {
     if (allPokemonNames?.length === 0) {
       getAllPokemonNames();
     }
+  }
+
+  // Just get all the names of the pokemon available from the api
+  // Can use this to check if a search is valid before calling the api again
+  async function getAllPokemonNames() {
+    let res = await axios.get(api.requestAllPokemon);
+    let results: AllPokemonResults[] = res.data.results;
+    let allPokemon: string[] = results.map((pokemon, index) => pokemon.name);
+    setAllPokemonNames(allPokemon);
   }
 
   function checkAndLoadFromUrlParams() {
@@ -50,57 +65,12 @@ const Home: FC = () => {
     }
   }
 
-  // Just get all the names of the pokemon available from the api
-  // Can use this to check if a search is valid before calling the api again
-  async function getAllPokemonNames() {
-    let res = await axios.get(api.requestAllPokemon);
-    let results: AllPokemonResults[] = res.data.results;
-    let allPokemon: string[] = results.map((pokemon, index) => pokemon.name);
-    setAllPokemonNames(allPokemon);
-  }
-
-  useEffect(() => {
-    checkAndLoadFromUrlParams();
-  }, [allPokemonNames, searchParams]);
-
   function filterPokemonByPageAndLoad(start: number) {
     let filtered = allPokemonNames.slice(start, start + RESULTS_PER_PAGE) ?? [];
     setLoadedPokemon(filtered);
   }
 
-  function nextPage() {
-    setSearchParams({
-      startFrom: (startFrom() + RESULTS_PER_PAGE).toString(),
-    });
-  }
-
-  function previousPage() {
-    setSearchParams({
-      startFrom: (startFrom() - RESULTS_PER_PAGE).toString(),
-    });
-  }
-
-  function searchHandler(value: string) {
-    setSearchParams({ query: value });
-  }
-
   async function searchPokemon(value: string) {
-    // let parsedNumber = +value; // Shorthand for parsing a number from a string
-    // if (value && (isNaN(parsedNumber) || parsedNumber === 0)) {
-    //   let filteredPokemon = filterPokemonBySearch(value);
-    //   if (filteredPokemon.length > 0) {
-    //     setLoadedPokemon(filteredPokemon);
-    //     setPokemonNotFound(false);
-    //   } else {
-    //     setPokemonNotFound(true);
-    //   }
-    // } else if (value && !isNaN(parsedNumber) && parsedNumber > 0) {
-    //   searchByNumber(parsedNumber);
-    // } else {
-    //   // No value, so just reset
-    //   setPokemonNotFound(false);
-    // }
-
     if (isStringANumber(value, false)) {
       searchByNumber(+value);
     } else {
@@ -153,6 +123,29 @@ const Home: FC = () => {
     }
   }
 
+  //#endregion
+
+  //#region Search Bar
+
+  function searchHandler(value: string) {
+    setSearchParams({ query: value });
+  }
+  //#endregion
+
+  //#region Pagination Buttons
+
+  function nextPage() {
+    setSearchParams({
+      startFrom: (startFrom() + RESULTS_PER_PAGE).toString(),
+    });
+  }
+
+  function previousPage() {
+    setSearchParams({
+      startFrom: (startFrom() - RESULTS_PER_PAGE).toString(),
+    });
+  }
+
   function showPaginationButton(nextButton: boolean): boolean {
     const resultsLength = allPokemonNames.length;
     if (nextButton) {
@@ -161,6 +154,8 @@ const Home: FC = () => {
       return startFrom() - RESULTS_PER_PAGE >= 0 ? true : false;
     }
   }
+
+  //#endregion
 
   return (
     <div className="flex flex-col items-center justify-center">
