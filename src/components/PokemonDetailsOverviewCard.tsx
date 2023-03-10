efde342cf775a9c021d7891660c80b7e9d9aa6d9
JoeGCode <122706533+JoeGCode@users.noreply.github.com>
2023-03-10 15:07:37 +0000

    Add desktop styling to the details page

--- src/components/PokemonDetailsOverviewCard.tsx
+++ src/components/PokemonDetailsOverviewCard.tsx
@@ -1,43 +1,66 @@
-import React, { CSSProperties, FC } from 'react'
-import { PokemonDetails, PokemonDetailsPokemon } from '../types'
-import { typeColours, typeLogo } from '../utils/pokemonTypeHelpers'
+import React, { CSSProperties, FC } from "react";
+import { PokemonDetails, PokemonDetailsPokemon } from "../types";
+import { typeColours, typeLogo } from "../utils/pokemonTypeHelpers";
 
 const PokemonDetailsOverviewCard: FC<PokemonDetailsPokemon> = ({ pokemon }) => {
+  const cardStyle: CSSProperties = {
+    backgroundColor: typeColours[pokemon.types[0]],
+    backgroundImage:
+      pokemon.types.length > 1
+        ? `linear-gradient(to top left, ${
+            typeColours[pokemon.types[1]]
+          }, 35%, transparent)`
+        : "none",
+  };
 
-    const cardStyle: CSSProperties = {
-        backgroundColor: typeColours[pokemon.types[0]],
-        backgroundImage: pokemon.types.length > 1 ? `linear-gradient(to top left, ${typeColours[pokemon.types[1]]}, 35%, transparent)` : 'none'
-    }
+  const imageURL =
+    pokemon.frontPokemonHomeSprite ??
+    pokemon.frontDreamWorldSpriteURL ??
+    pokemon.frontDefaultSpriteURL ??
+    pokemon.frontOfficialArtworkSprite;
 
-    const imageURL = ((pokemon.frontPokemonHomeSprite ?? pokemon.frontDreamWorldSpriteURL) ?? pokemon.frontDefaultSpriteURL) ?? pokemon.frontOfficialArtworkSprite;
-
-    return (
-        <div style={cardStyle} className='flex flex-col shadow-lg items-center justify-center p-4 rounded-2xl relative bg-slate-200 mt-28'>
-            <div className='absolute flex items-center justify-center -top-28 w-44 h-44'>
-                <img className='max-w-full max-h-full animate-[slight-bounce_2s_ease_infinite]' src={imageURL} alt={pokemon.name} />
-            </div>
-            <div className='flex flex-col gap-2 w-full items-center justify-center pt-12'>
-                <span className='text-lg'>{`#${pokemon.number}`}</span>
-                <span className='text-2xl capitalize'>{pokemon.name}</span>
-                <div className='flex justify-evenly w-full'>
-                    {pokemon.types.map((type, index) => {
-                        return <img className='w-20' src={typeLogo[type]} alt={type} key={index} />
-                    })}
-                </div>
-                <div className='flex items-center w-full justify-evenly'>
-                    <div className='flex flex-col items-center justify-center'>
-                        <span>Height:</span>
-                        <span>{pokemon.height / 10}m</span>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <span>Weight:</span>
-                        <span>{pokemon.weight / 10}kg</span>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div
+      style={cardStyle}
+      className="flex flex-col md:flex-row shadow-lg items-center justify-center p-4 rounded-2xl relative bg-slate-200 mt-28 md:mt-0"
+    >
+      <div className="absolute md:relative flex items-center justify-center -top-28 w-44 h-44 md:inset-0 md:h-64 lg:h-full md:w-full">
+        <img
+          className="h-auto max-h-full w-auto animate-[slight-bounce_2s_ease_infinite]"
+          src={imageURL}
+          alt={pokemon.name}
+        />
+      </div>
+      <div className="flex flex-col gap-2 w-full items-center justify-center pt-12 md:pt-0">
+        <span className="text-2xl text-center md:text-4xl capitalize">
+          {pokemon.name}
+        </span>
+        <span className="text-lg md:text-2xl">{`#${pokemon.number}`}</span>
+        <div className="flex justify-evenly w-full">
+          {pokemon.types.map((type, index) => {
+            return (
+              <img
+                className="w-20"
+                src={typeLogo[type]}
+                alt={type}
+                key={index}
+              />
+            );
+          })}
         </div>
-    )
-}
+        <div className="flex items-center w-full justify-evenly md:text-lg lg:text-xl">
+          <div className="flex flex-col items-center justify-center">
+            <span>Height:</span>
+            <span>{pokemon.height / 10}m</span>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <span>Weight:</span>
+            <span>{pokemon.weight / 10}kg</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PokemonDetailsOverviewCard
+export default PokemonDetailsOverviewCard;
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,87 +1,87 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import NotFoundCard from '../components/NotFoundCard';
-import PokemonDetailsOverviewCard from '../components/PokemonDetailsOverviewCard';
-import PokemonDetailsStatCard from '../components/PokemonDetailsStatCard';
-import api from '../services/api';
-import { PokemonDetails, PokeStat } from '../types';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NotFoundCard from "../components/NotFoundCard";
+import PokemonDetailsOverviewCard from "../components/PokemonDetailsOverviewCard";
+import PokemonDetailsStatCard from "../components/PokemonDetailsStatCard";
+import api from "../services/api";
+import { PokemonDetails, PokeStat } from "../types";
 
 const Detail = () => {
-    const { name } = useParams();
+  const { name } = useParams();
 
-    const [pokemon, setPokemon] = useState<PokemonDetails>();
-    const [isFound, setIsFound] = useState(true);
+  const [pokemon, setPokemon] = useState<PokemonDetails>();
+  const [isFound, setIsFound] = useState(true);
 
-    useEffect(() => {
-        getPokeDetails();
-    }, [name])
+  useEffect(() => {
+    getPokeDetails();
+  }, [name]);
 
+  async function getPokeDetails() {
+    // setIsLoading(true);
 
-    async function getPokeDetails() {
-        // setIsLoading(true);
+    try {
+      let detailResults = await axios.get(api.requestPokemon + name);
 
-        try {
-            let detailResults = await axios.get(api.requestPokemon + name);
+      if (detailResults.status === 200) {
+        let pokeDetails = detailResults.data;
 
-            if (detailResults.status === 200) {
-                let pokeDetails = detailResults.data;
+        let pokeObj: PokemonDetails = {
+          name: pokeDetails.name,
+          id: pokeDetails.id,
+          number: pokeDetails.id.toString().padStart(3, "0"),
+          frontDefaultSpriteURL: pokeDetails.sprites.front_default,
+          frontDreamWorldSpriteURL:
+            pokeDetails.sprites.other.dream_world.front_default,
+          frontPokemonHomeSprite: pokeDetails.sprites.other.home.front_default,
+          frontOfficialArtworkSprite:
+            pokeDetails.sprites.other["official-artwork"].front_default,
+          height: pokeDetails.height,
+          weight: pokeDetails.weight,
+          types: pokeDetails.types.map((type: any) => {
+            return type.type.name;
+          }),
+          hp: getStatValue(pokeDetails.stats, "hp"),
+          attack: getStatValue(pokeDetails.stats, "attack"),
+          defense: getStatValue(pokeDetails.stats, "defense"),
+          specialAttack: getStatValue(pokeDetails.stats, "special-attack"),
+          specialDefense: getStatValue(pokeDetails.stats, "special-defense"),
+          speed: getStatValue(pokeDetails.stats, "speed"),
+        };
 
-                let pokeObj: PokemonDetails = {
-                    name: pokeDetails.name,
-                    id: pokeDetails.id,
-                    number: pokeDetails.id.toString().padStart(3, '0'),
-                    frontDefaultSpriteURL: pokeDetails.sprites.front_default,
-                    frontDreamWorldSpriteURL: pokeDetails.sprites.other.dream_world.front_default,
-                    frontPokemonHomeSprite: pokeDetails.sprites.other.home.front_default,
-                    frontOfficialArtworkSprite: pokeDetails.sprites.other['official-artwork'].front_default,
-                    height: pokeDetails.height,
-                    weight: pokeDetails.weight,
-                    types: pokeDetails.types.map((type: any) => {
-                        return type.type.name
-                    }),
-                    hp: getStatValue(pokeDetails.stats, "hp"),
-                    attack: getStatValue(pokeDetails.stats, "attack"),
-                    defense: getStatValue(pokeDetails.stats, "defense"),
-                    specialAttack: getStatValue(pokeDetails.stats, "special-attack"),
-                    specialDefense: getStatValue(pokeDetails.stats, "special-defense"),
-                    speed: getStatValue(pokeDetails.stats, "speed"),
-                };
-
-                setPokemon(pokeObj);
-                setIsFound(true);
-            } else {
-                setIsFound(false);
-            }
-        } catch (error) {
-            console.log(error);
-            setIsFound(false);
-        }
-        // setIsLoading(false);
+        setPokemon(pokeObj);
+        setIsFound(true);
+      } else {
+        setIsFound(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsFound(false);
     }
+    // setIsLoading(false);
+  }
 
-    function getStatValue(stats: PokeStat[], statToGet: string): number {
-        return stats.find((stat) => stat.stat.name === statToGet)?.base_stat ?? 0;
-    }
+  function getStatValue(stats: PokeStat[], statToGet: string): number {
+    return stats.find((stat) => stat.stat.name === statToGet)?.base_stat ?? 0;
+  }
 
-    function renderDetailsPage(pokemon: PokemonDetails) {
-        return (
-            <div className='flex flex-col gap-6'>
-                <PokemonDetailsOverviewCard pokemon={pokemon} />
-                <PokemonDetailsStatCard pokemon={pokemon} />
-            </div>
-
-        );
-    }
-
+  function renderDetailsPage(pokemon: PokemonDetails) {
     return (
-        <div className='flex flex-col items-center justify-center'>
-        <div className='max-w-screen-2xl w-full'>
-            {pokemon && isFound && renderDetailsPage(pokemon)}
-            {!isFound && <NotFoundCard />}
-        </div>
-        </div>
-    )
-}
+      <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2">
+        <PokemonDetailsOverviewCard pokemon={pokemon} />
+        <PokemonDetailsStatCard pokemon={pokemon} />
+      </div>
+    );
+  }
 
-export default Detail
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="max-w-screen-2xl w-full">
+        {pokemon && isFound && renderDetailsPage(pokemon)}
+        {!isFound && <NotFoundCard />}
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
