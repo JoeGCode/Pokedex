f58f9692e7e8e05a82d5751ece8d5bff3f51c226
JoeGCode <122706533+JoeGCode@users.noreply.github.com>
2023-03-16 17:35:12 +0000

    Simple clear button
    Simple button to clear text in the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -14,15 +14,29 @@ const SearchBar: FC<SearchProps> = ({ searchHandler }) => {
     }
   }
 
+  function clearHandler() {
+    if (searchTerm !== "") {
+      setSearchTerm("");
+    }
+  }
+
   return (
     <div className="flex flex-col w-full justify-between mb-4 sm:gap-[5%] sm:flex-row">
-      <input
-        className="flex-1 rounded-md p-4 mb-2 sm:mb-0"
-        placeholder="Search Pokemon by name or number"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.currentTarget.value)}
-        onKeyUp={(e) => keyUpHandler(e)}
-      />
+      <div className="w-full relative flex-1">
+        <input
+          className="w-full rounded-md p-4 mb-2 sm:mb-0"
+          placeholder="Search Pokemon by name or number"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.currentTarget.value)}
+          onKeyUp={(e) => keyUpHandler(e)}
+        />
+        <button
+          className="absolute top-0 right-4 bottom-0 text-4xl"
+          onClick={() => clearHandler()}
+        >
+          X
+        </button>
+      </div>
       <button
         className="bg-green-400 rounded-md p-4 hover:bg-green-600"
         onClick={() => searchHandler(searchTerm.trim())}
