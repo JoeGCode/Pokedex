a68b15a24c1e92a703dc833b4fe79198235dcda1
JoeGCode <122706533+JoeGCode@users.noreply.github.com>
2023-03-16 15:27:46 +0000

    Update NotFoundCard.tsx
    I think discovered sounds better than found

--- src/components/NotFoundCard.tsx
+++ src/components/NotFoundCard.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const NotFoundCard = () => {
-    return (
-        <div className='flex flex-col w-full px-4 py-12 items-center justify-center text-center text-lg bg-slate-400 shadow-lg rounded-lg'>
-            This Pokemon does not exist... <br /> or hasn't been found yet!
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col w-full px-4 py-12 items-center justify-center text-center text-lg bg-slate-400 shadow-lg rounded-lg">
+      This Pokemon does not exist... <br /> or hasn't been discovered yet!
+    </div>
+  );
+};
 
-export default NotFoundCard
+export default NotFoundCard;
