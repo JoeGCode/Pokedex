4da60867be72d78baed46abf2a9847e72ebbc9e2
JoeGCode <122706533+JoeGCode@users.noreply.github.com>
2023-03-16 15:54:54 +0000

    Simple navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="text-white bg-[#131922] shadow-lg p-4 flex items-center justify-center w-full my-0 mx-auto">
+      <Link to="/">
+        <h1 className="text-6xl font-bold cursor-pointer">Pokédex</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
