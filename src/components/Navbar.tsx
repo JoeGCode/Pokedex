66be14f940e1c8177da8ee4e0c4799fa2a274192
JoeGCode <122706533+JoeGCode@users.noreply.github.com>
2023-03-17 14:24:33 +0000

    Styling changes
    Set the primary and secondary background colour in one place, so they can be easily changed and reused

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="text-white bg-[#131922] shadow-lg p-4 flex items-center justify-center w-full my-0 mx-auto">
+    <div className="text-white bg-bgSecondary shadow-lg p-4 flex items-center justify-center w-full my-0 mx-auto">
       <Link to="/">
         <h1 className="text-6xl font-bold cursor-pointer">Pokédex</h1>
       </Link>
--- src/components/PokemonDetailsStatCard.tsx
+++ src/components/PokemonDetailsStatCard.tsx
@@ -1,21 +1,45 @@
-import React, { FC } from 'react'
-import { PokemonDetailsPokemon } from '../types'
-import PokemonDetailsStatBarHorizontal from './PokemonDetailStatBarHorizontal'
+import React, { FC } from "react";
+import { PokemonDetailsPokemon } from "../types";
+import PokemonDetailsStatBarHorizontal from "./PokemonDetailStatBarHorizontal";
 
 const PokemonDetailsStatCard: FC<PokemonDetailsPokemon> = ({ pokemon }) => {
-    return (
-        <div className='flex flex-col shadow-lg items-center justify-center p-4 rounded-2xl bg-slate-400'>
-            <h2 className='text-2xl'>Stats</h2>
-            <div className='flex flex-col w-full items-center justify-center gap-2 mt-4'>
-                <PokemonDetailsStatBarHorizontal statName='HP' statValue={pokemon.hp} primaryType={pokemon.types[0]} />
-                <PokemonDetailsStatBarHorizontal statName='Attack' statValue={pokemon.attack} primaryType={pokemon.types[0]} />
-                <PokemonDetailsStatBarHorizontal statName='Defense' statValue={pokemon.defense} primaryType={pokemon.types[0]} />
-                <PokemonDetailsStatBarHorizontal statName='Special Attack' statValue={pokemon.specialAttack} primaryType={pokemon.types[0]} />
-                <PokemonDetailsStatBarHorizontal statName='Special Defense' statValue={pokemon.specialDefense} primaryType={pokemon.types[0]} />
-                <PokemonDetailsStatBarHorizontal statName='Speed' statValue={pokemon.speed} primaryType={pokemon.types[0]} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col shadow-lg items-center justify-center p-4 rounded-2xl  bg-bgSecondary">
+      <h2 className="text-2xl text-white">Stats</h2>
+      <div className="flex flex-col w-full items-center justify-center gap-2 mt-4">
+        <PokemonDetailsStatBarHorizontal
+          statName="HP"
+          statValue={pokemon.hp}
+          primaryType={pokemon.types[0]}
+        />
+        <PokemonDetailsStatBarHorizontal
+          statName="Attack"
+          statValue={pokemon.attack}
+          primaryType={pokemon.types[0]}
+        />
+        <PokemonDetailsStatBarHorizontal
+          statName="Defense"
+          statValue={pokemon.defense}
+          primaryType={pokemon.types[0]}
+        />
+        <PokemonDetailsStatBarHorizontal
+          statName="Special Attack"
+          statValue={pokemon.specialAttack}
+          primaryType={pokemon.types[0]}
+        />
+        <PokemonDetailsStatBarHorizontal
+          statName="Special Defense"
+          statValue={pokemon.specialDefense}
+          primaryType={pokemon.types[0]}
+        />
+        <PokemonDetailsStatBarHorizontal
+          statName="Speed"
+          statValue={pokemon.speed}
+          primaryType={pokemon.types[0]}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default PokemonDetailsStatCard
+export default PokemonDetailsStatCard;
